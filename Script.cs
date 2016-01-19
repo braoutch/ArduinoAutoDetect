1c2359ee0dd3776469bd4aadd7ad0b73f83c3b45
braoutch <antoinelassagne@gmail.com>
2016-01-19 17:17:19 +0100

    Improvements
    - Sees every SerialPort on every OS
    - Test them and open the one it can (can take a few seconds)
    - Still crashes while reading what they have to say…

--- Script.cs
+++ Script.cs
@@ -1,57 +1,83 @@
-﻿using UnityEngine;
+﻿//IF ITS NOT WORKING TRY TO USE MONO 2.0 OR 2.0SUBSET !!!
+using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using System.IO.Ports;
 using System;
 
-public class Script : MonoBehaviour {
-
-	public int i=0;
-	public SerialPort[] streamTest = new SerialPort[SerialPort.GetPortNames ().Length];
-	public string serialReadTest;
-	public string goodPort;
+public class Script : MonoBehaviour
+{
+	public SerialPort goodStream;
 	public string arduinoCode = "code";
+	public List<SerialPort> serialPortList = new List<SerialPort> ();
+	int portfermes = 0;
 
 	// Use this for initialization
-	void Awake () {
-
+	void Awake ()
+	{
+		SerialPort stream;
 		var devices = SerialPort.GetPortNames ();
+	
+		if (devices.Length == 0) {
+			// try manual enumeration
+			devices = System.IO.Directory.GetFiles ("/dev/");
+			Debug.Log ("No ports found with GetPortNames");
+		}
+	
 		Debug.Log (devices.Length + " port(s) série trouvé(s)");
+		
 		for (int j = 0; j < devices.Length; j++) {
 			Debug.Log (devices [j]);
 		}
 		//On tente d'ouvrir tous les ports
-		foreach (string thisDevice in devices) {
+		for (int i = 0; i < devices.Length; i++) {
+
 			try {
-				streamTest [i] = new SerialPort (thisDevice, 115200);
-				streamTest [i].Open ();
-				if (streamTest [i].IsOpen) {
-					Debug.Log ("Le port " + i + " est ouvert !");	
-				}
-				Debug.Log ("initialisation réussie pour i = " + i);
-				try{
-					serialReadTest = streamTest [i].ReadLine ();
-				}
-				catch(Exception f)
-				{
-					Debug.Log("oh bah ça marche pas");
-				}
-				if (serialReadTest == arduinoCode) {
-					Debug.Log ("MUHAHAHAHAHA c'est le bon code");
-					goodPort = thisDevice;
-					} else {
-						streamTest [i].Close ();
-						Debug.Log ("Mauvais code !");
-					}
-					} catch (Exception e) {
-						Debug.Log ("raté pour i = " + i);
-					}
-					i++;
-				}
+				stream = new SerialPort (devices [i], 115200);
+				stream.Open ();
+			} catch (Exception e) {
+				portfermes++;
+				continue;
+			}
+			if (stream.IsOpen) {
+				Debug.Log ("Le port " + i + " est ouvert , son nom est " + devices [i]);
+				serialPortList.Add (stream);
+			} else
+				continue;
+			Debug.Log ("initialisation réussie pour i = " + i);
+		
+
+		}
 		//On teste tous les ports qu'on a réussi à ouvrir
+		Debug.Log (portfermes + " ports fermés.");
 	}
-	
+
+	public void Detecter ()
+	{
+		string serialReadTest;
+		for (int i = 0; i < serialPortList.Count; i++) {
+			
+			try {
+				serialReadTest = serialPortList [i].ReadLine ();
+			} catch (Exception f) {
+				Debug.Log ("oh bah ça marche pas");
+				continue;
+			}
+			if (serialReadTest == arduinoCode) {
+				Debug.Log ("MUHAHAHAHAHA c'est le bon code");
+				goodStream = serialPortList [i];
+				break;
+
+			} else {
+				serialPortList [i].Close ();
+				Debug.Log ("Mauvais code !");
+			}
+		}
+	}
+
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 
 	}
 }
